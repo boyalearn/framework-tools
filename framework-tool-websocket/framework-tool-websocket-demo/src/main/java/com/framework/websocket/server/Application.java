610a922a7bdcc7642c496bdb9a82f65fcea9c23c
zouhuixing <2114517411@qq.com>
2021-04-13 23:12:42 +0800

    将工程发布到git仓库使用

--- framework-tool-websocket/framework-tool-websocket-demo/src/main/java/com/framework/websocket/server/Application.java
+++ framework-tool-websocket/framework-tool-websocket-demo/src/main/java/com/framework/websocket/server/Application.java
@@ -1,14 +1,21 @@
 package com.framework.websocket.server;
 
+import com.broheim.websocket.spring.annonation.EnableWebSocketServer;
+import com.broheim.websocket.spring.reactor.CommandReactor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.framework.websocket.server"})
+@EnableWebSocketServer
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public CommandReactor commandReactor(){
+        return new CommandReactor();
+    }
 }
